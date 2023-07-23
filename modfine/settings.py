4b22c620f351e9cd7a816b3d36a7569e13668d52
Arunarchitect <intformbase@gmail.com>
2023-07-23 22:52:05 +0530

    SQLLite to Postgres

--- modfine/settings.py
+++ modfine/settings.py
@@ -85,11 +85,14 @@ WSGI_APPLICATION = 'modfine.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'postgres',
+        'PASSWORD': 'Dream123',
+        'HOST': '136.244.102.215',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
